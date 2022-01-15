308c929a2f3ee52f19d640abca71238bfe0490af
Manuel Gozzi <gozzigg@gmail.com>
2022-01-15 12:13:39 +0100

    First commit;

---
+++ src/main/java/it/manuelgozzi/blog/ManuelGozziBlogApplication.java
@@ -0,0 +1,13 @@
+package it.manuelgozzi.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuelGozziBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManuelGozziBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/manuelgozzi/blog/ManuelGozziBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package it.manuelgozzi.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuelGozziBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
